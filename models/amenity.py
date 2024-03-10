9c2c0f0550ac18edd9266c0a07177196e52e43bb
Aviwe <aviwenkambule@gmail.com>
2024-03-10 05:54:26 -0700

    AirBnB_clone

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module creates a Amenity class"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Represent an amenity"""
+
+    name = ""
---
+++ tests/test_models/test_engine/__init__.py
@@ -0,0 +1,4 @@
+^C
+^c
+c
+root@b07098725e01:~/AirBnB_clone/tests/test_models/test_engine# ls
